6962ad022111af55424face85e8c7d620c34db19
vmanakov <vmanakov@phoenixit.ru>
2023-07-28 20:02:33 +0300

    readme.md

--- backend/db/categories.js
+++ backend/db/categories.js
@@ -3,5 +3,5 @@ export default [
     {id: 2, title: "Игры"},
     {id: 3, title: "Продукты питания"},
     {id: 4, title: "Одежда"},
-    {id: 5, title: "Вещи Олега"},
+    {id: 5, title: "Вещи Олега 2"},
 ]
